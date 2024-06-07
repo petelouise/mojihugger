06f2d5fc8c07690dafb577a58b9c6e935ab1dc70
petelouise <programmetress@petelouise.beauty>
2024-06-06 19:25:52 -0500

    Add logging statements for css addition and markdown post processor registration.

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 import { Plugin } from "obsidian";
 
 function addCss(): void {
+	console.log("adding css");
 	const style = document.createElement("style");
 	style.textContent = `
 	.emoji {
@@ -14,10 +15,10 @@ export default class Mojihugger extends Plugin {
 	styleElement: HTMLStyleElement;
 
 	onload() {
-		console.log("Loading Custom Hanging Indent Plugin");
+		console.log("~~~~~mojihugger has arrived~~~~~");
 		// Inject custom CSS for hanging indent
 		addCss();
-
+		console.log("registering markdown post processor");
 		this.registerMarkdownPostProcessor((element, context) => {
 			const emojiRegex = /([\u{1F600}-\u{1F64F}])/gu;
 			element.innerHTML = element.innerHTML.replace(
